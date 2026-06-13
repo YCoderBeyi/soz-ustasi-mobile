11c2e9af86427c384bf060ffce864a996261745e
Yasar Derebaşı <yasar.derebasi@sancaketiket.com>
2026-06-13 09:21:13 +0300

    chore: remove all game files from git tracking, retain only privacy policy page

--- src/game/normalize.ts
+++
@@ -1,8 +0,0 @@
-export function normalizeWord(word: string) {
-  return word
-    .trim()
-    .toLocaleUpperCase('tr-TR')
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replaceAll('\u0130', 'I');
-}
